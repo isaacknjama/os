d24e36e82eb443cbce01aa774bb153d12a139678
okjodom <okjodom@gmail.com>
2025-02-04 06:31:21 +0300

    feat: env configurable log levels

--- libs/common/src/logger/logger.module.ts
+++ libs/common/src/logger/logger.module.ts
@@ -1,22 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 
 @Module({
   imports: [
-    PinoLoggerModule.forRoot({
-      pinoHttp: {
-        transport: {
-          target: 'pino-pretty',
-          options: {
-            singleLine: true,
+    PinoLoggerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        pinoHttp: {
+          transport: {
+            target: 'pino-pretty',
+            options: {
+              singleLine: true,
+            },
           },
+          redact: {
+            paths: ['req.headers', 'res.headers'],
+            remove: true,
+          },
+          level: configService.get<string>('LOG_LEVEL', 'info'),
         },
-        redact: {
-          paths: ['req.headers', 'res.headers'],
-          remove: true,
-        },
-      },
+      }),
+      inject: [ConfigService],
     }),
   ],
+  exports: [PinoLoggerModule],
 })
 export class LoggerModule {}
