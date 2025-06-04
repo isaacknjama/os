bd08746a518804a8e86dd2de555c32870556a0d2
okjodom <okjodom@gmail.com>
2025-06-04 06:29:48 +0300

    Revert "feat: setup infra for bitsaccoserver"
    This reverts commit 9d402047496fd456bbfc07a033ee40fe2944efee.

--- apps/bitsaccoserver/src/infrastructure/security/csrf.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class CsrfService {
-  // CSRF service implementation
-}
--- apps/bitsaccoserver/src/infrastructure/security/encryption.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class EncryptionService {
-  // Encryption service implementation
-}
