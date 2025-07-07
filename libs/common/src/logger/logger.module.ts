ccf214ab7e3c650de4cf72c18949fd340afee6ee
okjodom <okjodom@gmail.com>
2025-07-08 01:33:17 +0300

    feat: use pino json logging in prod

--- libs/common/src/logger/logger.module.ts
+++ libs/common/src/logger/logger.module.ts
@@ -5,21 +5,31 @@ import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
 @Module({
   imports: [
     PinoLoggerModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        pinoHttp: {
-          transport: {
-            target: 'pino-pretty',
-            options: {
-              singleLine: true,
+      useFactory: (configService: ConfigService) => {
+        const isProduction =
+          configService.get<string>('NODE_ENV') === 'production';
+
+        return {
+          pinoHttp: {
+            // Only use pino-pretty transport in development
+            ...(isProduction
+              ? {}
+              : {
+                  transport: {
+                    target: 'pino-pretty',
+                    options: {
+                      singleLine: true,
+                    },
+                  },
+                }),
+            redact: {
+              paths: ['req.headers', 'res.headers'],
+              remove: true,
             },
+            level: configService.get<string>('LOG_LEVEL', 'info'),
           },
-          redact: {
-            paths: ['req.headers', 'res.headers'],
-            remove: true,
-          },
-          level: configService.get<string>('LOG_LEVEL', 'info'),
-        },
-      }),
+        };
+      },
       inject: [ConfigService],
     }),
   ],
